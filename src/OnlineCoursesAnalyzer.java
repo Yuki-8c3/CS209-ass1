0ceace6face2dfc69ffb67cf63e0c1c5477862fb
yuki <12012640@mail.sustech.edu.cn>
2023-03-28 17:01:35 +0800

    #3 : according to Google Checks, edit some parts

--- src/OnlineCoursesAnalyzer.java
+++ src/OnlineCoursesAnalyzer.java
@@ -10,7 +10,8 @@ import java.util.stream.Stream;
 
 /**
  *
- * This is just a demo for you, please run it on JDK17 (some statements may be not allowed in lower version).
+ * This is just a demo for you, please run it on JDK17
+ * (some statements may be not allowed in lower version).
  * This is just a demo, and you can extend and implement functions
  * based on this demo, or implement it in a different way.
  */
@@ -19,19 +20,24 @@ public class OnlineCoursesAnalyzer {
     List<Course> courses = new ArrayList<>();
 
     public OnlineCoursesAnalyzer(String datasetPath) {
-        BufferedReader br = null;
-        String line;
-        try {
-            br = new BufferedReader(new FileReader(datasetPath, StandardCharsets.UTF_8));
-            br.readLine();
-            while ((line = br.readLine()) != null) {
-                String[] info = line.split(",(?=([^\\\"]*\\\"[^\\\"]*\\\")*[^\\\"]*$)", -1);
-                Course course = new Course(info[0], info[1], new Date(info[2]), info[3], info[4], info[5],
-                        Integer.parseInt(info[6]), Integer.parseInt(info[7]), Integer.parseInt(info[8]),
-                        Integer.parseInt(info[9]), Integer.parseInt(info[10]), Double.parseDouble(info[11]),
-                        Double.parseDouble(info[12]), Double.parseDouble(info[13]), Double.parseDouble(info[14]),
-                        Double.parseDouble(info[15]), Double.parseDouble(info[16]), Double.parseDouble(info[17]),
-                        Double.parseDouble(info[18]), Double.parseDouble(info[19]), Double.parseDouble(info[20]),
+    BufferedReader br = null;
+    String line;
+    try {
+        br = new BufferedReader(new FileReader(datasetPath, StandardCharsets.UTF_8));
+        br.readLine();
+        while ((line = br.readLine()) != null) {
+            String[] info = line.split(",(?=([^\\\"]*\\\"[^\\\"]*\\\")*[^\\\"]*$)", -1);
+            Course course = new Course(info[0], info[1], new Date(info[2]), info[3], info[4], info[5],
+                        Integer.parseInt(info[6]), Integer.parseInt(info[7]),
+                    Integer.parseInt(info[8]),
+                        Integer.parseInt(info[9]), Integer.parseInt(info[10]),
+                    Double.parseDouble(info[11]),
+                        Double.parseDouble(info[12]), Double.parseDouble(info[13]),
+                    Double.parseDouble(info[14]),
+                        Double.parseDouble(info[15]), Double.parseDouble(info[16]),
+                    Double.parseDouble(info[17]),
+                        Double.parseDouble(info[18]), Double.parseDouble(info[19]),
+                    Double.parseDouble(info[20]),
                         Double.parseDouble(info[21]), Double.parseDouble(info[22]));
                 this.courses.add(course);
             }
@@ -50,11 +56,12 @@ public class OnlineCoursesAnalyzer {
 
     //1
     public Map<String, Integer> getPtcpCountByInst() {
-        /**
-         * This method returns a <institution, count> map, where the key is the institution while the value is the
-         * total number of participants who have accessed the courses of the institution.
-         * The map should be sorted by the alphabetical order of the institution
-         */
+    /**
+    * This method returns a <institution, count> map, where the key is the institution
+    * while the value is the
+    * total number of participants who have accessed the courses of the institution.
+    * The map should be sorted by the alphabetical order of the institution
+    * */
         Map<String, Integer> result = new HashMap<>();
 
         //ParticipantsByInstitution: first group the participants and count their sums
@@ -71,14 +78,14 @@ public class OnlineCoursesAnalyzer {
 
     //2
     public Map<String, Integer> getPtcpCountByInstAndSubject() {
-        /**
-         * This method returns a <institution-course Subject, count> map, where the key is the string
-         * concatenating the Institution and the course Subject (without quotation marks) using '-'
-         * while the value is the total number of participants in a course Subject of an institution.
-         * The map should be sorted by descending order of count (i.e., from most to least participants).
-         * If two participants have the same count, then they should be sorted by the alphabetical order of the
-         * institution-course Subject
-         */
+    /**
+     * This method returns a <institution-course Subject, count> map, where the key is the string
+     * concatenating the Institution and the course Subject (without quotation marks) using '-'
+     * while the value is the total number of participants in a course Subject of an institution.
+     * The map should be sorted by descending order of count (i.e., from most to least participants).
+     * If two participants have the same count, then they should be sorted by the alphabetical order of the
+     * institution-course Subject
+     */
         Map<String, Integer> result = new HashMap<>();
 
         //PtcpByInstAndSubject: 1)concat using '-' 2)value is # of participants
